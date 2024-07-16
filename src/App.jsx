e7c04d84b71b35f04c2ccd232d59f3051ae1a6a6
Nadika <nadika.name@gmail.com>
2024-07-16 10:33:19 +0100

    feat: add styles

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from 'react';
+import './App.css';
+import DigitalClock from './DigitalClock';
+
+function App() {
+    return (
+    <>
+     <DigitalClock />
+    </>
+  )
+}
+
+export default App;
---
+++ src/DigitalClock.jsx
@@ -0,0 +1,11 @@
+import React, { useState, useEffect } from 'react';
+
+function DigitalClock() {
+    return (
+        <div className="DigitalClock">
+            <div className="clock"><span>00:00:00</span></div>
+        </div>
+    )
+}
+
+export default DigitalClock;
